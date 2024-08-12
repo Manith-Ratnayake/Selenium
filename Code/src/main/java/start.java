9ad77c59073e9d875e341d99a17605cf324d9be5
Manith Ratnayake <manithbbratnayake@gmail.com>
2024-08-12 16:18:42 +0530

    Adding more automation

--- Code/src/main/java/start.java
+++ Code/src/main/java/start.java
@@ -1,16 +1,17 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
+
 
 public class start {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -28,7 +29,12 @@ public class start {
 
         WebElement location = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("a7dc8ec444")));
         location.click();
-        location.sendKeys("Colombo");
+
+        WebElement locationSearch = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ada65db9b5")));
+        locationSearch.sendKeys("Colombo");
+
+        WebElement date = driver.findElement(By.className("c3953d2910"));
+        date.click();
 
 
     }
